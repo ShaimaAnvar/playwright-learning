74c1ac475b54ba7cbc883699aae356689df76058
shaima <shaima.anvar@gmail.com>
2026-09-23 21:52:00 -0400

    browser context and Page fixtures

---
+++ tests/UIBasicstest.spec.js
@@ -0,0 +1,12 @@
+ const {test}= require('@playwright/test');
+test('Browser context playwright test', async ({browser})=>{
+    const context  = await browser.newContext;
+    const page =await context.newPage();
+   await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+
+});
+test('Page playwrite test', async ({page})=>{
+    
+   await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+
+});
