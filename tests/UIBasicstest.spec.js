28ca096ce7c374c19f7bdb8ae073852cdf33d77e
shaima <shaima.anvar@gmail.com>
2026-09-25 22:11:29 -0400

    Locators supported by playwright and how to type into elements on page

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -3,6 +3,9 @@ test('Browser context playwright test', async ({browser})=>{
     const context  = await browser.newContext();
     const page =await context.newPage();
    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+   await page.locator('#username').fill('rahulshetty');
+   await page.locator('#password').fill('Learning@830$3mK2');
+   await page.locator('#signInBtn').click();
    console.log(await page.title());
 
 });
