066abe2f2990b78e5d93e5e5c30d49bc230678d2
shaima <shaima.anvar@gmail.com>
2026-09-25 21:46:58 -0400

    Setup Assertions in Playwright tests & multiple browsers Configuration

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,8 @@ const config = ({
   },
   reporter: 'html',
   use: {
-    browserName: 'chromium',
+    browserName: 'webkit',
+    headless: false,
   }
   
 });
--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,12 +1,16 @@
- const {test}= require('@playwright/test');
+ const {test, expect}= require('@playwright/test');
 test('Browser context playwright test', async ({browser})=>{
     const context  = await browser.newContext();
     const page =await context.newPage();
    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+   console.log(await page.title());
 
 });
-test.only('Page playwrite test', async ({page})=>{
+test('Page playwrite test', async ({page})=>{
     
    await page.goto('https://google.com');
+   console.log(await page.title());
+   await expect(page).toHaveTitle("Google");
+
 
 });
